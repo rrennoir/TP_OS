46fb4fb2b2310b424b40a090182c069d10dd951b
Ryan Rennoir <ryan.rennoir@student.unamur.be>
2019-06-27 00:38:38 +0200

    fix tab in exo5.c

--- TP1.2/exo5/exo5.c
+++ TP1.2/exo5/exo5.c
@@ -5,14 +5,14 @@ void palidrome(char string[4096], int length)
     int i, j;
 
     for(i = length - 1, j = 0; i >= 0, j <= length - 1; i--, j++)
+    {
+        if (string[i] != string[j])
         {
-            if (string[i] != string[j])
-            {
-                printf("The string isn't a palindrome.\n");
-                return;
-            }
+            printf("The string isn't a palindrome.\n");
+            return;
         }
-        printf("The string is a palidrome.\n");
+    }
+    printf("The string is a palidrome.\n");
 }
 
 void main(void)
