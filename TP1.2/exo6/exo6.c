5e2ca17a440bfe4aa0a787594a584c99fb231401
Ryan Rennoir <ryan.rennoir@student.unamur.be>
2019-06-27 00:48:14 +0200

    Change makefile and exo6.c

--- TP1.2/exo6/exo6.c
+++ TP1.2/exo6/exo6.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 
-void main(int argc, char *argv[]){
-
+void main(int argc, char *argv[])
+{
     int i = 0, j, length = 0;
-    
-    while (argv[1][i] != '\0'){
+
+    if (argc != 2)
+    {
+        printf("Error: No argument given (1 string required)\n");
+        return;
+    }
+
+    while (argv[1][i] != '\0')
+    {
         length ++;
         i ++;
     }
 
     for(i = length - 1, j = 0; i >= 0, j <= length - 1; i--, j++)
     {
-        if (argv[1][i] != argv[1][j]){
+        if (argv[1][i] != argv[1][j])
+        {
             printf("The string isn't a palindrome.\n");
             return;
         }
