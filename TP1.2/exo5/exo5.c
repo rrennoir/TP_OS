0d6b21324c6cea59b585b3b2960acfdd5deaf121
Ryan Rennoir <ryan.rennoir@student.unamur.be>
2019-06-27 00:38:09 +0200

    Change Makefile and exo5.

--- TP1.2/exo5/exo5.c
+++ TP1.2/exo5/exo5.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-void palidrome(char string[50], int length){
+void palidrome(char string[4096], int length)
+{
     int i, j;
 
     for(i = length - 1, j = 0; i >= 0, j <= length - 1; i--, j++)
         {
-            if (string[i] != string[j]){
+            if (string[i] != string[j])
+            {
                 printf("The string isn't a palindrome.\n");
                 return;
             }
@@ -13,14 +15,16 @@ void palidrome(char string[50], int length){
         printf("The string is a palidrome.\n");
 }
 
-void main(void){
+void main(void)
+{
     int i = 0, length = 0;
-    char string[50];
+    char string[4096];
 
     printf("Enter a string.\n");
-    scanf("%49s", string);
+    scanf("%4095s", string);
 
-    while (string[i] != '\0'){
+    while (string[i] != '\0')
+    {
         length ++;
         i ++;
     }
