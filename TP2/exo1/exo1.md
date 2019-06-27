efa8a6a96de67bd8d20b514d010a4f22842b088c
Ryan Rennoir <ryan.rennoir@student.unamur.be>
2019-06-27 03:01:06 +0200

    Change mistere.c and finish exo1.md

--- TP2/exo1/exo1.md
+++ TP2/exo1/exo1.md
@@ -1,13 +1,14 @@
 # TP 2: Exo1
 
-## 1)Que fait ce programme ?
+## 1) Que fait ce programme
 
-* Place 5 numbres introduit pas l'utilisateur dans une liste de 5 éléments, si le nombre est négatif il est stocké comme nombre négatif.
+* il enregistre des nombres positif ou négative entré par l'utilisateur dans une list.
 
-## 2) En utilisant le gdb, mettez en évidence le passage par référence de chaque élément du tableau.
+## 2) En utilisant le gdb, mettez en évidence le passage par référence de chaque élément du tableau
 
-* ?
+* Dans `for (n = 0; n < SIZE && getint(&array[n]) != EOF; n++);`
+* `&array[n]` est un passage en référence de l'élement n du tableau.
 
-## 3)Essayez de comprendre les rapports de la commande strace.
+## 3)Essayez de comprendre les rapports de la commande strace
 
-* ?
+* Strace permet de voir tous les appels systeme que le programe execute.
--- TP2/exo1/mistere.c
+++ TP2/exo1/mistere.c
@@ -7,38 +7,49 @@ void ungetch(int);
 
 #define SIZE 5
 
-int main() {
-  printf("test\n");
+int main()
+{
+    int n, array[SIZE], getint(int *);
   
-  int n, array[SIZE], getint(int *);
-  
-  for (n = 0; n < SIZE && getint(&array[n]) != EOF; n++)
-       ;
-  return 0;
+    for (n = 0; n < SIZE && getint(&array[n]) != EOF; n++)
+    {
+        // To better understand what happend.
+        for (int i = 0; i < SIZE; i++)
+        {
+            if (i <= n)
+            {
+                printf("array[%d] = %d\n", i, array[i]);
+            }
+            else
+            {
+                printf("array[%d] = not initialized\n", i);
+            }
+        }
+    }
+    return 0;
 }
 
 int getint(int *pn)
-{
-  printf("test\n");
+{    
+    int c, sign;
+    
+    while (isspace(c = getchar()));
+    if (!isdigit(c) && c != EOF && c != '+' && c != '-') 
+    {
+        ungetc(c, stdin);
+        return 0;
+    }
+  
+    sign = (c == '-') ? -1 : 1;
+    if (c == '+' || c == '-')
+        c = getchar();
+  
+    for (*pn = 0; isdigit(c); c = getchar())
+        *pn = 10 * *pn + (c - '0');
+        *pn *= sign;
+  
+    if (c != EOF)
+        ungetc(c, stdin);
 
-  int c, sign;
-  while (isspace(c = getchar()))
-           ;
-  if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
-    ungetc(c, stdin);
-    return 0;
-  }
-  
-  sign = (c == '-') ? -1 : 1;
-  if (c == '+' || c == '-')
-    c = getchar();
-  
-  for (*pn = 0; isdigit(c); c = getchar())
-    *pn = 10 * *pn + (c - '0');
-    *pn *= sign;
-  
-  if (c != EOF)
-    ungetc(c, stdin);
-
-  return c; 
+    return c; 
 }
